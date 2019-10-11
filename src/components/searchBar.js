b70fae1cd44db2ad42eb55064b28b17428aa5087
Matt Readout <matt.readout@gmail.com>
2019-10-11 13:40:13 -0400

    create dummy onSubmit handler

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import InputGroup from 'react-bootstrap/InputGroup'
+import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 
@@ -16,19 +17,26 @@ class SearchBar extends React.Component {
 		this.setState({ searchTerm })
 	}
 
+	handleSubmit = (event) => {
+		event.preventDefault()
+		console.log('submited!', this.state.searchTerm);
+	}
+
 	render() {
 		return (
 			<Row>
 				<Col>
-					<InputGroup>
-						<FormControl
-							placeholder='Search'
-							value={this.state.searchTerm}
-							onChange={this.handleChange}/>
-						<InputGroup.Append>
-							<Button>Search</Button>
-						</InputGroup.Append>
-					</InputGroup>
+					<Form onSubmit={this.handleSubmit}>
+						<InputGroup>
+							<FormControl
+								placeholder='Search'
+								value={this.state.searchTerm}
+								onChange={this.handleChange}/>
+							<InputGroup.Append>
+								<Button variant='secondary' type='submit'>Search</Button>
+							</InputGroup.Append>
+						</InputGroup>
+					</Form>
 				</Col>
 			</Row>
 		)
