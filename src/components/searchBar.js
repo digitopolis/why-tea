98cb0d53a68e8d6b3cec320ac91bd577152c5c10
Matt Readout <matt.readout@gmail.com>
2019-10-11 13:13:53 -0400

    create/render dummy searchBar

---
+++ src/components/searchBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const SearchBar = (props) => {
+	return (
+		<Row>
+			<Col><h1>search bar here</h1></Col>
+		</Row>
+	)
+}
+
+export default SearchBar
