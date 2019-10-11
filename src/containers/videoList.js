67e384527db665ec74a20638220c6666a9578c42
Matt Readout <matt.readout@gmail.com>
2019-10-11 17:20:07 -0400

    receive search results and render videoItems

--- src/containers/videoList.js
+++ src/containers/videoList.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import VideoItem from '../components/videoItem'
 
-const VideoList = (props) => {
+const VideoList = ({ videos }) => {
 	return (
-		<Col>Video List</Col>
+		<Col>
+			<h1>Video List</h1>
+			{videos.map(video => {
+				return (
+					<Row>
+						<VideoItem {...video} />
+					</Row>
+				)
+			})}
+		</Col>
 	)
 }
 
