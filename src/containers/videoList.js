d434a84a475415ccb30a09176b7a1a0105b38932
Matt Readout <matt.readout@gmail.com>
2019-10-11 16:43:04 -0400

    create videoList and videoPlayer

---
+++ src/containers/videoList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Col from 'react-bootstrap/Col'
+
+const VideoList = (props) => {
+	return (
+		<Col>Video List</Col>
+	)
+}
+
+export default VideoList
---
+++ src/containers/videoPlayer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
+const VideoPlayer = (props) => {
+	return (
+		<Col xs={12} md={8}>
+			<Row>Video Player</Row>
+			<Row>Details</Row>
+		</Col>
+	)
+}
+
+export default VideoPlayer
