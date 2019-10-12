5045c8eec3ed7ca5259585a06dd5bfd9270ec3df
Matt Readout <matt.readout@gmail.com>
2019-10-12 16:59:17 -0400

    fetch video data when selected

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { YOUTUBE } from './apiEndpoints'
+import { YTSEARCH, ONEVIDEO } from './apiEndpoints'
 import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container'
 import SearchBar from './components/searchBar'
@@ -11,20 +11,25 @@ class App extends React.Component {
 
 	state = {
 		searchResults: [],
-		selectedVideo: null
+		selectedVideo: null,
+		videoDetails: {}
 	}
 
 	handleSubmit = async (searchTerm) => {
 		const apiKey = process.env.REACT_APP_GOOGLE_API_KEY
-		const data = await fetch(`${YOUTUBE}q=${searchTerm}&key=${apiKey}`).then(res => res.json())
+		const data = await fetch(`${YTSEARCH}q=${searchTerm}&key=${apiKey}`).then(res => res.json())
 		this.setState({
 			searchResults: data.items
 		})
 	}
 
-	handleVideoSelect = (videoId) => {
-		const selectedVideo = videoId
-		this.setState({ selectedVideo })
+	handleVideoSelect = async (videoId) => {
+		const apiKey = process.env.REACT_APP_GOOGLE_API_KEY
+		const details = await fetch(`${ONEVIDEO}id=${videoId}&key=${apiKey}`).then(res => res.json())
+		this.setState({
+			selectedVideo: videoId,
+		 	videoDetails: details.items[0]
+		})
 	}
 
 	render() {
@@ -34,7 +39,7 @@ class App extends React.Component {
 					<SearchBar handleSubmit={this.handleSubmit}/>
 				</Row>
 				<Row className='justify-content-center'>
-					<VideoContainer />
+					{this.state.selectedVideo ? <VideoContainer details={this.state.videoDetails}/> : null }
 					<VideoList
 						videos={this.state.searchResults}
 						handleVideoSelect={this.handleVideoSelect}
--- src/apiEndpoints.js
+++ src/apiEndpoints.js
@@ -1,1 +1,2 @@
-export const YOUTUBE = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&'
+export const YTSEARCH = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&'
+export const ONEVIDEO = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&'
