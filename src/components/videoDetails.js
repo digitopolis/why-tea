fb15d11c3abdf4612e1749bdeec2f2d1ae24baf9
Matt Readout <matt.readout@gmail.com>
2019-10-12 16:09:13 -0400

    render video data from props

--- src/components/videoDetails.js
+++ src/components/videoDetails.js
@@ -3,9 +3,14 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 const VideoDetails = (props) => {
+	const dateUploaded = new Date(props.snippet.publishedAt)
 	return (
 		<Row>
-			<h1>title here</h1>
+			<h2>{props.snippet.title}</h2>
+			<p>{dateUploaded.toLocaleString()}</p>
+			<hr />
+			<h6>{props.snippet.channelTitle}</h6>
+			<p>{props.snippet.description}</p>
 		</Row>
 	)
 }
