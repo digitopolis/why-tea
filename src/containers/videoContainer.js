49a869adabd47e3b35f69677566cc3c6f51244f6
Matt Readout <matt.readout@gmail.com>
2019-10-12 15:47:21 -0400

    add data to render and pass to details component

--- src/containers/videoContainer.js
+++ src/containers/videoContainer.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { details } from '../testingData'
 import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
+// import Row from 'react-bootstrap/Row'
 import VideoPlayer from '../components/videoPlayer'
+import VideoDetails from '../components/videoDetails'
 
 const VideoContainer = (props) => {
 	return (
 		<Col xs={12} md={8}>
 			<VideoPlayer videoId={'K-r7FRpZReo'}/>
-			<Row>Details</Row>
+			<VideoDetails {...details}/>
 		</Col>
 	)
 }
---
+++ src/testingData.js
@@ -0,0 +1,57 @@
+export const details = {
+
+   kind: "youtube#video",
+   etag: "\"p4VTdlkQv3HQeTEaXgvLePAydmU/cewglo1z6AOTuZRsRLBt9vwhKwU\"",
+   id: "K-r7FRpZReo",
+   snippet: {
+    publishedAt: "2019-10-10T21:51:58.000Z",
+    channelId: "UCqAbcgEp4NqCRnLG3NVJezw",
+    title: "懐かしいおもちゃを回す猫がかわいい   Cute cats playing with NOSTALGIC toys.",
+    description: "懐かしのハンドスピナーを回すデコ豆がかわいいです＾＾\nひのきも昔はグルグル回してたんですけどねぇ～落ち着きましたね♬\n\n♡いつも動画をご覧いただき、ありがとうございます。\nひのき、ひまわり、秀吉、豆大福、オデコのかわいい動画をほぼ毎日更新してるので、気に入って頂ければ是非チャンネル登録をお願いします。\n\n【チャンネル登録】⇒https://www.youtube.com/c/ひのき猫\n\n【ひのきたちの写真集が発売されました！】⇒https://amzn.to/2XuCYb7\n\n\nひのき猫のアメブロ、ツイッター、インスタグラムetcを一つにまとめたサイトを作っていただきました＾＾\nたま～に遊びに来てくれると嬉しいです♬\n\n【エンタマ】⇒https://hinoki-cat.entama.app/\n\n【LINEスタンプ作ってみました】⇒https://store.line.me/stickershop/product/1884525/ja\n\n#豆大福\n#オデコ\n#ひのき猫",
+    thumbnails: {
+     default: {
+      "url": "https://i.ytimg.com/vi/K-r7FRpZReo/default.jpg",
+      "width": 120,
+      "height": 90
+     },
+     "medium": {
+      "url": "https://i.ytimg.com/vi/K-r7FRpZReo/mqdefault.jpg",
+      "width": 320,
+      "height": 180
+     },
+     "high": {
+      "url": "https://i.ytimg.com/vi/K-r7FRpZReo/hqdefault.jpg",
+      "width": 480,
+      "height": 360
+     },
+     "standard": {
+      "url": "https://i.ytimg.com/vi/K-r7FRpZReo/sddefault.jpg",
+      "width": 640,
+      "height": 480
+     },
+     "maxres": {
+      "url": "https://i.ytimg.com/vi/K-r7FRpZReo/maxresdefault.jpg",
+      "width": 1280,
+      "height": 720
+     }
+    },
+    channelTitle: "ひのき猫",
+    "tags": [
+     "ねこ",
+     "cat",
+     "cats",
+     "cute",
+     "かわいい",
+     "マンチカン",
+     "ハンドスピナー"
+    ],
+    "categoryId": "15",
+    "liveBroadcastContent": "none",
+    "defaultLanguage": "ja",
+    "localized": {
+     "title": "懐かしいおもちゃを回す猫がかわいい   Cute cats playing with NOSTALGIC toys.",
+     "description": "懐かしのハンドスピナーを回すデコ豆がかわいいです＾＾\nひのきも昔はグルグル回してたんですけどねぇ～落ち着きましたね♬\n\n♡いつも動画をご覧いただき、ありがとうございます。\nひのき、ひまわり、秀吉、豆大福、オデコのかわいい動画をほぼ毎日更新してるので、気に入って頂ければ是非チャンネル登録をお願いします。\n\n【チャンネル登録】⇒https://www.youtube.com/c/ひのき猫\n\n【ひのきたちの写真集が発売されました！】⇒https://amzn.to/2XuCYb7\n\n\nひのき猫のアメブロ、ツイッター、インスタグラムetcを一つにまとめたサイトを作っていただきました＾＾\nたま～に遊びに来てくれると嬉しいです♬\n\n【エンタマ】⇒https://hinoki-cat.entama.app/\n\n【LINEスタンプ作ってみました】⇒https://store.line.me/stickershop/product/1884525/ja\n\n#豆大福\n#オデコ\n#ひのき猫"
+    },
+    "defaultAudioLanguage": "ja"
+   }
+  }
