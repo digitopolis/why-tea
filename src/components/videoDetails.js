ec46e02741fc7be119e477858abbc1357ed34efe
Matt Readout <matt.readout@gmail.com>
2019-10-12 15:46:54 -0400

    create videoDetails

---
+++ src/components/videoDetails.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const VideoDetails = (props) => {
+	return (
+		<Row>
+			<h1>title here</h1>
+		</Row>
+	)
+}
+
+export default VideoDetails
