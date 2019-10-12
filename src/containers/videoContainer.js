811df627deab0bde61625c4cf17c2b8f9d16c4d5
Matt Readout <matt.readout@gmail.com>
2019-10-12 17:03:49 -0400

    pass video details down to player/details

--- src/containers/videoContainer.js
+++ src/containers/videoContainer.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import { details } from '../testingData'
 import Col from 'react-bootstrap/Col'
-// import Row from 'react-bootstrap/Row'
 import VideoPlayer from '../components/videoPlayer'
 import VideoDetails from '../components/videoDetails'
 
 const VideoContainer = (props) => {
 	return (
 		<Col xs={12} md={8}>
-			<VideoPlayer videoId={'K-r7FRpZReo'}/>
-			<VideoDetails {...details}/>
+			<VideoPlayer videoId={props.details.id}/>
+			<VideoDetails {...props.details}/>
 		</Col>
 	)
 }
